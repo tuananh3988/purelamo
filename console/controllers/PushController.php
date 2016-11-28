f9186c4f53149824aec3b04e59fd9e27b80e74d9
Tuan anh - seta <tuananh3988@gmail.com>
2016-11-28 18:18:13 +0700

    fire base update

--- console/controllers/PushController.php
+++ console/controllers/PushController.php
@@ -12,7 +12,17 @@ class PushController extends \yii\console\Controller
      */
     public function actionIndex()
     {
-        $notify = Notification::find()->where(['status' => 1, 'delete_flag' => 0])->andWhere('reserve_date > ' . date())
+        $notify = Notification::find()->where(['status' => 0, 'delete_flag' => 0])->andWhere('reserve_date <= "' . date('Y-m-d H:i:s') . '"')->all();
+        foreach ($notify as $n) {
+            $res = [
+                'data' => [
+                    'title' => $n['subject'],
+                    'message' => $n['message'],
+                    'timestamp' => date('Y-m-d G:i:s'),
+                ]
+            ];
+        }
+        var_dump($notify[0]->id);
         echo 'a';
         return 0;
     }
